ec16fca738d548e0bae454c4fe5a2aa9c3d6a0b6
LucLenne <luc.lenne@outlook.fr>
2025-11-05 20:15:26 +0100

    movement good

--- Assets/Teams/CruiserTeam/Scripts/Action/MoveToCluster.cs
+++ Assets/Teams/CruiserTeam/Scripts/Action/MoveToCluster.cs
@@ -10,48 +10,61 @@ namespace Cruiser
     [TaskCategory("CruiserTeam")]
     public class MoveToCluster : Action
     {
-        private InputData inputData;
         public AnimationCurve thrustAnglePower;
+        private Coroutine followRoutine;
+
+        public override void OnStart()
+        {
+            followRoutine = StartCoroutine(FollowPath());
+        }
+
         public override TaskStatus OnUpdate()
         {
+            if (followRoutine != null)
+            {
+                return TaskStatus.Running;
+            }
+            else
+            {
+                return TaskStatus.Success;
+            }
+        }
+        IEnumerator FollowPath()
+        {
+            //Init Value
             GameData data = CruiserController.Instance.GameData;
             SpaceShipView spaceship = CruiserController.Instance.SpaceShipView;
-
             List<Vector2> path = BestCluster().OptimalTrajectory(spaceship);
-
             for (int i = 0; i < path.Count; i++)
             {
                 Vector2 currentTarget = path[i];
-                bool changed;
 
+                bool changed;
                 do
                 {
                     changed = false;
-
                     foreach (AsteroidView asteroid in data.Asteroids)
                     {
                         if (SegmentCircleIntersection(spaceship.Position, currentTarget, asteroid.Position, asteroid.Radius))
                         {
                             currentTarget = AvoidAsteroid(asteroid, spaceship, currentTarget);
-                            changed = true; // Re-check, il peut encore taper un autre astéroïde
+                            changed = true;
                         }
                     }
+                }
+                while (changed);
 
-                } while (changed); // boucle tant qu’on trouve d'autres collisions
-
-                path[i] = currentTarget;
-                StartCoroutine(MoveTo(currentTarget, spaceship, data));
+                yield return StartCoroutine(MoveTo(currentTarget, spaceship, data));
             }
-            return TaskStatus.Success;
+            followRoutine = null;
+            yield break;
         }
-
 
         WayPointCluster BestCluster()
         {
             WayPointCluster closerCluster = new WayPointCluster();
             float minDistance = float.MaxValue;
             List<WayPointCluster> clusters = CruiserController.Instance.Clusters;
-            WayPointCluster clusterDone = new WayPointCluster();
             foreach (WayPointCluster cluster in clusters)
             {
                 float currentDistance = Vector2.Distance(cluster.averagePos, CruiserController.Instance.SpaceShipView.Position);
@@ -66,20 +79,32 @@ namespace Cruiser
 
         IEnumerator MoveTo(Vector2 target, SpaceShipView spaceship, GameData data)
         {
+            bool hasReached = false;
             float radius = spaceship.Radius + data.WayPoints[0].Radius;
-            while (Vector2.Distance(spaceship.Position, target) > radius)
+
+            while (!hasReached)
             {
                 float targetPos = AimingHelpers.ComputeSteeringOrient(spaceship, target);
                 float thrust = ComputeThurst(spaceship, target);
-                CruiserController.Instance.inputData = new InputData(thrust, targetPos, false, false, false);
+
+                CruiserController.Instance.inputData =
+                    new InputData(thrust, targetPos, false, false, false);
+
+                // Détection : si le vaisseau est passé dans le cercle
+                if (Vector2.Distance(spaceship.Position, target) < radius)
+                    hasReached = true;
+
                 yield return null;
             }
-            yield return TaskStatus.Running;
+
+            // on quitte la coroutine
+            yield break;
         }
 
         float ComputeThurst(SpaceShipView spaceship, Vector2 target)
         {
-            float angle = Vector2.Angle(spaceship.Velocity, target - spaceship.Position);
+
+            float angle = Vector2.Angle(spaceship.LookAt, target - spaceship.Position);
             return thrustAnglePower.Evaluate(angle);
         }
 
@@ -95,20 +120,36 @@ namespace Cruiser
 
         Vector2 AvoidAsteroid(AsteroidView asteroid, SpaceShipView spaceship, Vector2 target)
         {
+            Vector2 toAst = asteroid.Position - spaceship.Position;
             float avoidRadius = spaceship.Radius + asteroid.Radius;
-            float dist = avoidRadius * 1.5f;
 
-            Vector2 shipPos = spaceship.Position;
-            Vector2 dir = (target - shipPos).normalized;
+            // Distance actuelle
+            float d = toAst.magnitude;
 
-            // Deux points de contournement possibles
-            Vector2 perp = new Vector2(dir.y, -dir.x) * dist;
+            // Si déjà trop proche : esquive d’urgence
+            if (d < avoidRadius * 1.2f)
+            {
+                Vector2 escapeDir = (spaceship.Position - asteroid.Position).normalized;
+                return spaceship.Position + escapeDir * avoidRadius * 2f;
+            }
 
-            Vector2 c1 = asteroid.Position + perp;
-            Vector2 c2 = asteroid.Position - perp;
+            // Angle tangent
+            float angleOffset = Mathf.Acos(avoidRadius / d);
 
-            // On choisit celui qui rapproche le plus de la target finale
-            return (c1 - target).sqrMagnitude < (c2 - target).sqrMagnitude ? c1 : c2;
+            // Direction générale
+            float baseAngle = Mathf.Atan2(toAst.y, toAst.x);
+
+            // Deux options
+            Vector2 p1 = asteroid.Position + new Vector2(
+                Mathf.Cos(baseAngle + angleOffset),
+                Mathf.Sin(baseAngle + angleOffset)) * avoidRadius * 1.2f;
+
+            Vector2 p2 = asteroid.Position + new Vector2(
+                Mathf.Cos(baseAngle - angleOffset),
+                Mathf.Sin(baseAngle - angleOffset)) * avoidRadius * 1.2f;
+
+            // Choisir celui qui va le plus vers la target
+            return Vector2.Distance(p1, target) < Vector2.Distance(p2, target) ? p1 : p2;
         }
     }
 }
